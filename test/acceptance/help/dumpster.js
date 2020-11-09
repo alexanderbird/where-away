ebc512f13b9e0dcb87f0aa0bfc9d2bcce0ac7d96
Alexander Bird <alex@alexanderbird.software>
2020-11-08 21:21:53 -0800

    Add notes about the provocatively named 'dumpster' file

--- test/acceptance/help/dumpster.js
+++ test/acceptance/help/dumpster.js
@@ -1,3 +1,30 @@
+/*
+ * The Dumpster. 
+ *
+ * This class is the dumping ground for gross utility methods that make tests
+ * easier to read. 
+ *
+ * "But why use a name with such a negative connotation?"
+ *
+ * Because this file is Bad™. It's a coupling magnet -- it ties all these tests
+ * together. These test utility files are troublesome in any context, but
+ * particularly so with these browser UI tests. "Dumpster" reminds me that this
+ * is a bad place to put things I care about. I'd like to find a better way,
+ * but I don't know of one yet. So, I call it what it is. No pretending that
+ * this is a Good Idea.
+ *
+ * As a point of information, I wouldn't use a provocative word like "Dumpster"
+ * in a professional setting, but I would do what I can to avoid these files
+ * growing and growing. If there's a better pattern available I may mark the
+ * dumpster file (usually called "TestUtilities" or something polite and vague)
+ * as deprecated so folks know they should do something better.
+ *
+ * If you read this and have better test helper architecture patterns or ideas,
+ * please send me a tweet (@alexander_bird) or email
+ * (alex@alexanderbird.software) -- I'm keen to improve the state of my
+ * practice or just discuss the (anti)pattern with another practitioner.
+ *
+ */
 module.exports = {
   click: async function click(page, _text) {
     await page.evaluate(text => {
