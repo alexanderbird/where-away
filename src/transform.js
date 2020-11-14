4217fda089f5de2dafbd6165972579e9be1f780d
Alexander Bird <alex@alexanderbird.software>
2020-11-13 16:18:58 -0800

    Add transformer to convert bookmark data to pre-rendered anchors sorted by keyboard shortcut paths

---
+++ src/transform.js
@@ -0,0 +1,29 @@
+function transform(data) {
+  const result = {};
+  function addToResult(path, html) {
+    result[path] = (result[path] || '') + html;
+  }
+
+  data.forEach(d => chomp('', addToResult, d));
+
+  return result;
+}
+
+function chomp(path, addToResult, { key, href, label, children }) {
+  if (children) {
+    addToResult(path, `<a data-keyboard-shortcut="${key}" onclick="addToPath(this)">${label}</a>`);
+    children.forEach(child => chomp(path + key, addToResult, child));
+  } else {
+    const parameterData = href.match(/^(.*){{(.*)}}(.*)$/);
+
+    if (parameterData) {
+      const [_, left, parameterLabel, right] = parameterData;
+      const html = `<a data-keyboard-shortcut="${key}" onclick="navigateTo(param => \`${left}\${param}${right}\`)" data-parameter-label="${parameterLabel.trim()}">${label}</a>`;
+      addToResult(path, html);
+    } else {
+      addToResult(path, `<a data-keyboard-shortcut="${key}" href="${href}">${label}</a>`);
+    }
+  }
+}
+
+module.exports = { transform };
---
+++ test/unit/transform.spec.js
@@ -0,0 +1,62 @@
+const { transform } = require('../../src/transform');
+
+describe('transform', () => {
+  it('produces an html string with anchor tags', () => {
+    const input = [{ href: 'https://foo.com', key: 'a', label: 'First Link' }];
+    const actual = transform(input);
+    expect(actual['']).toEqual('<a data-keyboard-shortcut="a" href="https://foo.com">First Link</a>');
+  });
+
+  it('produces an anchor tag for group nodes', () => {
+    const input = [{ href: 'https://foo.com/{{ Favourite Fruit or Appliance }}/search?where=here', key: 'f', label: 'Fruits and Appliances' }];
+    const actual = transform(input);
+    expect(actual['']).toEqual('<a data-keyboard-shortcut="f" onclick="navigateTo(param => \`https://foo.com/${param}/search?where=here\`)" data-parameter-label="Favourite Fruit or Appliance">Fruits and Appliances</a>');
+  });
+
+  it('produces an anchor tag for parameterized links', () => {
+    const input = [{ children: [], key: 'g', label: 'A Group of Bookmarks' }];
+    const actual = transform(input);
+    expect(actual['']).toEqual('<a data-keyboard-shortcut="g" onclick="addToPath(this)">A Group of Bookmarks</a>');
+  });
+
+  it('produces an anchor tag for each root link', () => {
+    const input = [
+      { href: 'https://foo.com', key: 'f', label: 'The Foo' },
+      { href: 'https://bar.com', key: 'b', label: 'BARBAR' },
+      { href: 'https://baz.com', key: 'z', label: 'le bazzoo' }
+    ];
+    const actual = transform(input);
+    expect(actual['']).toEqual(
+        '<a data-keyboard-shortcut="f" href="https://foo.com">The Foo</a>'
+      + '<a data-keyboard-shortcut="b" href="https://bar.com">BARBAR</a>'
+      + '<a data-keyboard-shortcut="z" href="https://baz.com">le bazzoo</a>'
+    );
+  });
+
+  describe('nested links', () => {
+    const input = [
+      { key: '1', label: 'Grandparent', children: [
+        { key: 'a', label: 'Parent A', children: [
+          { key: 'm', label: 'Child Group', children: [
+            { href: 'https://cool.io', key: '1', label: 'Link 1' },
+          ] }
+        ] },
+        { key: 'b', label: 'Parent B', children: [
+          { href: 'https://example.com', key: 'm', label: 'Link M' },
+        ] }
+      ] }
+    ];
+
+    it('produces objects keys for each unique path', () => {
+      const actual = transform(input);
+
+      expect(Object.keys(actual).sort()).toEqual(['', '1', '1a', '1am', '1b'])
+    });
+
+    it('produces the right html for nested paths', () => {
+      const actual = transform(input);
+
+      expect(actual['1am']).toEqual('<a data-keyboard-shortcut="1" href="https://cool.io">Link 1</a>');
+    });
+  });
+});
