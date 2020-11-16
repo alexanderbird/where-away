8daee4ca9e5ad94feb41d2769e331b80c1b8814c
Alexander Bird <alex@alexanderbird.software>
2020-11-16 09:36:50 -0800

    Add more logging to github netlify action; trivial change to demo to test deploy

--- .github_workflow_scripts/release_netlify_build.sh
+++ .github_workflow_scripts/release_netlify_build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 AUTHORIZATION="Authorization: Bearer $NETLIFY_TOKEN"
+echo Determine the id of the latest deploy
 deploy_id="$(curl -H "$USER_AGENT" -H "$AUTHORIZATION" $NETLIFY_API/sites/$SITE_ID/deploys | jq -r ".[0].id")"
-curl -H "$USER_AGENT" -H "$AUTHORIZATION" -X POST $NETLIFY_API/sites/$SITE_ID/deploys/$deploy_id/restore
-curl -H "$USER_AGENT" -H "$AUTHORIZATION" -X POST $NETLIFY_API/deploys/$deploy_id/lock
+echo "  ▸ $deploy_id"
+echo "Enable publishing deploy $deploy_id"
+curl -H "$USER_AGENT" -H "$AUTHORIZATION" -X POST $NETLIFY_API/sites/$SITE_ID/deploys/$deploy_id/restore && echo "  ▸ success"
+echo "Disable publishing any deploys beyond $deploy_id"
+curl -H "$USER_AGENT" -H "$AUTHORIZATION" -X POST $NETLIFY_API/deploys/$deploy_id/lock && "  ▸ success"
